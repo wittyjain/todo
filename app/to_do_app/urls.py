492178c07e0c62d10b551cf16fbcf30f6c131239
Abhishek Jain <abhishek.jain@cowrks.com>
2020-07-31 13:20:12 +0530

    Added post url for tasks

--- app/to_do_app/urls.py
+++ app/to_do_app/urls.py
@@ -1,11 +1,14 @@
 from app import app
-from app.to_do_app.views import TaskList
+from app.to_do_app.views import TaskItem
 
-task_list = TaskList.as_view('tasks')
+task_item = TaskItem.as_view('tasks')
 
 app.add_url_rule('/tasks', defaults={'task_id': None},
-                 view_func=task_list, methods=['GET', 'POST']
+                 view_func=task_item, methods=['GET']
                  )
-app.add_url_rule('/tasks/<string:task_id>', view_func=task_list,
+app.add_url_rule('/tasks',
+                 view_func=task_item, methods=['POST']
+                 )
+app.add_url_rule('/tasks/<string:task_id>', view_func=task_item,
                  methods=['GET', 'PUT', 'DELETE']
                  )
