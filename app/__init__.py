f88aa3d1848730c96e9ba4c76f28080eb0db6239
Abhishek Jain <abhishek.jain@cowrks.com>
2020-07-31 13:20:49 +0530

    Added flask migration tool for handling migrations

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.health_check import todo_app
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object('config')
 app.register_blueprint(todo_app)
 db = SQLAlchemy(app)
-
-from app.to_do_app.urls import *
-
 db.create_all()
+migrate = Migrate(app, db)
+from app.to_do_app.urls import *
 
